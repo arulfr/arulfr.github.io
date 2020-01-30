dc80033242215ad3e52a0723f1b1e742f94bcb82
arulfr <amrullahfr@gmail.com>
2020-01-29 23:58:47 -0800

    deploy

---
+++ precache-manifest.d0bbb0533b0e3b262c8f205f935b7dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419272d99c6a18eec355df5100d9177b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03835b43ddb7d10c130f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4cd3f16f0ad682fc27bf",
+    "url": "/static/js/2.0ec8c90a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0ec8c90a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03835b43ddb7d10c130f",
+    "url": "/static/js/main.ec2b8454.chunk.js"
+  },
+  {
+    "revision": "fe42ca3722ecebecd61a",
+    "url": "/static/js/runtime-main.a07bf254.js"
+  }
+]);
